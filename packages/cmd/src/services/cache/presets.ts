bedf01a9a90b624085d26871a7d710072fe21c01
DJ Mountney <dj@currents.dev>
2024-09-24 13:59:35 -0700

    chore: Add GitLab preset

--- packages/cmd/src/services/cache/presets.ts
+++ packages/cmd/src/services/cache/presets.ts
@@ -4,23 +4,73 @@ import { PW_CONFIG_DUMP_FILE } from "../../commands/cache/options";
 import { CacheGetCommandConfig } from "../../config/cache";
 import { getCI } from "../../env/ciProvider";
 import { writeFileAsync } from "../../lib";
+import { MetaFile } from "./lib";
 
-export async function handleLastRunPreset(
+export async function handlePreLastRunPreset(
   config: CacheGetCommandConfig,
-  ci: ReturnType<typeof getCI>
+  ci: ReturnType<typeof getCI>,
 ) {
   switch (ci.provider) {
-    case "GITHUB_ACTIONS":
+    case "githubActions":
       await dumpPWConfigForGHA(config, ci);
+      break;
+    case "gitlab":
+      await dumpPwConfigForGitlab(config, ci);
       break;
     default:
       break;
   }
 }
 
+export async function handlePostLastRunPreset(
+  config: CacheGetCommandConfig,
+  ci: ReturnType<typeof getCI>,
+  meta: MetaFile,
+) {
+  switch (ci.provider) {
+    case "gitlab":
+      await dumpPwConfigForGitlab(config, ci, meta);
+      break;
+    default:
+      break;
+  }
+}
+
+async function dumpPwConfigForGitlab(
+  config: CacheGetCommandConfig,
+  ci: ReturnType<typeof getCI>,
+  meta: MetaFile | null = null,
+) {
+  const previousRunAttempt = meta
+    ? parseIntSafe(meta.ci.params?.runAttempt, 0)
+    : 0;
+  const runAttempt = previousRunAttempt + 1;
+  const nodeIndex = parseIntSafe(process.env.CI_NODE_INDEX, 1);
+  const jobTotal = parseIntSafe(process.env.CI_NODE_TOTAL, 1);
+
+  const lastFailedOption = runAttempt > 1 ? "--last-failed" : "";
+
+  let shardOption = "";
+  if (jobTotal > 1) {
+    shardOption =
+      runAttempt > 1 ? "--shard=1/1" : `--shard=${nodeIndex}/${jobTotal}`;
+  }
+
+  const pwCliOptions = [lastFailedOption, shardOption]
+    .filter(Boolean)
+    .join(" ");
+
+  await writeFileAsync(
+    config.pwConfigDump ?? PW_CONFIG_DUMP_FILE,
+    `EXTRA_PW_FLAGS="${pwCliOptions}"
+RUN_ATTEMPT="${runAttempt}"
+`,
+  );
+}
+
 async function dumpPWConfigForGHA(
   config: CacheGetCommandConfig,
-  ci: ReturnType<typeof getCI>
+  ci: ReturnType<typeof getCI>,
 ) {
   const runAttempt = parseIntSafe(ci.params.GITHUB_RUN_ATTEMPT, 1);
   const nodeIndex = parseIntSafe(process.env.GH_STRATEGY_NODE_INDEX, 1);
@@ -37,12 +87,15 @@ async function dumpPWConfigForGHA(
   const pwCliOptions = [lastFailedOption, shardOption]
     .filter(Boolean)
     .join(" ");
-  await writeFileAsync(config.pwConfigDump ?? PW_CONFIG_DUMP_FILE, pwCliOptions);
+  await writeFileAsync(
+    config.pwConfigDump ?? PW_CONFIG_DUMP_FILE,
+    pwCliOptions,
+  );
 }
 
 const parseIntSafe = (
   value: string | undefined,
-  defaultValue: number
+  defaultValue: number,
 ): number => {
   const parsed = _.toNumber(value);
   return _.isNaN(parsed) ? defaultValue : parsed;
